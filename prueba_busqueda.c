1e98d0467bd5d201e855c575bb1fa2acaa71f9c1
unityofdisaster2 <rrosalesr1500@alumno.ipn.mx>
2018-09-16 16:53:02 -0500

    se elimina conflicto

--- prueba_busqueda.c
+++ prueba_busqueda.c
@@ -42,11 +42,6 @@ int main(int argc, char **argv)
         default:
            break;
     }
-<<<<<<< HEAD
-    
-=======
-    printf("cambio sutil amiguito");
->>>>>>> eef7dace3395a51ae74422c1d6240066e676f9df
     return 0;
 }
 
